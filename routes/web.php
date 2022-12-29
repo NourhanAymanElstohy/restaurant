2b6687905d2038f8c1aa948646918a904ac8a21a
NourhanAymanElstohy <nour7anelsto7y@gmail.com>
2022-12-29 09:57:43 +0200

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\MenuController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'menus'], function () {
+    Route::get('/', [MenuController::class, 'index'])->name('menus.index');
+    Route::get('/{menu}/edit', [MenuController::class, 'edit'])->name('menus.edit');
+    Route::put('/{menu}', [MenuController::class, 'update'])->name('menus.update');
+
+    Route::get('/create', [MenuController::class, 'create'])->name('menus.create');
+    Route::post('/', [MenuController::class, 'store'])->name('menus.store');
+
+    Route::get('/{menu}', [MenuController::class, 'show'])->name('menus.show');
+    Route::delete('/{menu}', [MenuController::class, 'destroy'])->name('menus.delete');
+});
